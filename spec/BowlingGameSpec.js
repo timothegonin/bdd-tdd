0726c2c7f40e97acd3f0bf4377bd459c3130afc6
TimotheGonin <gonin.timothe@gmail.com>
2023-11-06 10:22:26 +0100

    basic suits and specs

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,13 @@
+describe("Bowling game", function () {
+	it("should properly calculate a gutter game", function () {
+		//test
+	});
+
+	it("should properly calculate a strike", function () {
+		//test
+	});
+
+	it("should properly calculate a spare", function () {
+		//test
+	});
+});
