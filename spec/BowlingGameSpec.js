35334ba53cafbf2d397cb1d5800eed8e4239e391
TimotheGonin <gonin.timothe@gmail.com>
2023-11-07 11:12:11 +0100

    bowlingGame code + first test to green

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,7 +1,19 @@
-describe("Bowling game", function () {
+describe("BowlingGame", function () {
+	var game;
+
+	beforeEach(function () {
+		game = new BowlingGame();
+	});
+
+	function rollMany(n, pins) {
+		for (var i = 0; i < n; i++) {
+			game.roll(pins);
+		}
+	}
+
 	it("should properly calculate a gutter game", function () {
 		rollMany(20, 0);
-		expect(game.score().toEqual(0));
+		expect(game.score()).toEqual(0);
 	});
 
 	it("should properly calculate a strike", function () {
