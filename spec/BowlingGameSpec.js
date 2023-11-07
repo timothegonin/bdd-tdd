5072d846b1309d4e03a6192fda99678308e01e15
TimotheGonin <gonin.timothe@gmail.com>
2023-11-07 09:18:28 +0100

    basic expectations

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,6 +1,7 @@
 describe("Bowling game", function () {
 	it("should properly calculate a gutter game", function () {
-		//test
+		rollMany(20, 0);
+		expect(game.score().toEqual(0));
 	});
 
 	it("should properly calculate a strike", function () {
